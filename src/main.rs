98f82450b3af6a4edaba661ee9d71678ba8516f4
Tero Vuorela <tero.vuorela@iki.fi>
2023-09-13 06:39:08 +0300

    Impl reading line by line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 
 use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -14,24 +16,40 @@ struct Cli {
     debug: bool,
 }
 
-fn main() {
+// TODO own error handling for file not found
+fn main() -> std::io::Result<()> {
     let args = Cli::parse();
-
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
     if args.debug {
         println!("DEBUG: Reading file {:?}", args.path);
         println!("DEBUG: Matching string {:?}", args.pattern);
     }
+    //let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    let f = File::open(&args.path)?;
+    let mut reader = BufReader::new(f);
+
     let mut any_match = false;
-    for line in content.lines() {
+    let mut line = String::new();
+    while let Ok(len) = reader.read_line(&mut line) {
+        if len == 0 {
+            break;
+        }
+        if args.debug {
+            println!("DEBUG read {} bytes", len);
+        }
         if line.contains(&args.pattern) {
             println!("{}", line);
             any_match = true;
         }
+
+        // reset buffer
+        line.truncate(0);
     }
 
     if !any_match {
         println!("No matches");
     }
+
+    Ok(())
 }
