84843b186b7cb02271f69c36c75ae694e59b0e36
Tero Vuorela <tero.vuorela@iki.fi>
2023-09-18 07:08:19 +0300

    Impl debug_logger module
    Including impl for file based logging. Core of rchat doesn't yet use
    this module.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ use toml::value::Table;
 
 use crate::chatgpt_request::chatgpt_request;
 use crate::cli::Cli;
+use crate::debug_logger::{DebugLogger, EmptyDebugLogger, FileDebugLogger, StdoutDebugLogger};
 use crate::settings::settings;
 
 mod cli;
-
+mod debug_logger;
 mod chatgpt_request;
 mod settings;
 
@@ -23,6 +24,9 @@ mod settings;
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::parse();
+
+    let debug = build_debug_logger(&args);
+    debug.debug(&"Starting...");
 
     let prompt = match &args.prompt {
         Some(prompt) => prompt.clone(),
@@ -39,7 +43,20 @@ async fn main() -> Result<()> {
     let settings = settings(&args)?;
 
     chatgpt_request(&prompt, &settings.chatgpt).await;
-    println!("Done");
+
+    println!("Exiting...");
 
     Ok(())
+}
+
+fn build_debug_logger(args: &Cli) -> Box<dyn DebugLogger> {
+    if args.debug {
+        if let Some(debug_file) = &args.debug_file {
+            Box::new(FileDebugLogger::new(debug_file))
+        } else {
+            Box::new(StdoutDebugLogger)
+        }
+    } else {
+        Box::new(EmptyDebugLogger)
+    }
 }
